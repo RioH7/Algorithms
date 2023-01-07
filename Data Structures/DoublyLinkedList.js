792066fd1bb8a09b925fe9c460bd3117a2859849
RioH7 <humphreysrio1@aol.com>
2023-01-07 07:38:34 +0000

    Added Doubly Linked List Class

---
+++ Data Structures/DoublyLinkedList.js
@@ -0,0 +1,101 @@
+class Node { 
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+    setNextNode(node) {
+        if(node instanceof Node || node === null) {
+            this.next = node;
+        } else {
+            throw new Error('Next node must be a member of the Node class.')
+        }
+        this.next = node;
+    }
+    setPreviousNode(node) {
+        if(node instanceof Node || node === null) {
+            this.previous = node;
+        } else {
+            throw new Error('Previous node must be a member of the Node class.')
+        }
+    }
+    getNextNode() {
+        return this.next;
+    }
+    getPreviousNode() {
+        return this.previous;
+    }
+}
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+    addToHead(data) {
+        const newHead = new Node(data);
+
+        if(this.head !== null) {
+            newHead.setNextNode(this.head);
+            this.head.setPreviousNode(newHead);
+        }
+        this.head = newHead;
+        if(this.tail === null) {
+            this.tail = this.head;
+        }
+    }
+    addToTail(data) {
+        const newTail = new Node(data);
+        
+        if(this.tail !== null) {
+            newTail.setPreviousNode(this.tail);
+            this.tail.setNextNode(newTail);
+        }
+        this.tail = newTail;
+        if(this.head === null) {
+            this.head = this.tail;
+        }
+    }
+    removeHead() {
+        if(this.head === null) return;
+
+        this.head = this.head.getNextNode();
+
+        if(this.head !== null) {
+            this.head.setPreviousNode(null); 
+        } else {
+            this.removeTail();
+        }
+    }
+    removeTail() {
+        if(this.tail === null) return;
+        this.tail = this.tail.getPreviousNode();
+        if(this.tail !== null) {
+            this.tail.setNextNode(null);
+        } else {
+            this.removeHead();
+        }
+    }
+    removeByData(data) {
+        if(this.head === null) return;
+        let currentNode = this.head;
+
+        while(currentNode !== null && currentNode.data !== data) {
+            currentNode = currentNode.getNextNode();
+        }
+        if(currentNode === null) return null;
+        if(currentNode === this.head) this.removeHead();
+        if(currentNode === this.tail) this.removeTail();
+        currentNode.getPreviousNode().setNextNode(currentNode.getNextNode());
+        currentNode.getNextNode().setPreviousNode(currentNode.getPreviousNode());
+    }
+    printList() {
+        let currentNode = this.head;
+        let output = '<head> ';
+        while (currentNode !== null) {
+          output += currentNode.data + ' ';
+          currentNode = currentNode.getNextNode();
+        }
+        output += '<tail>';
+        console.log(output);
+    }
+}
